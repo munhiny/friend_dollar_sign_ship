db96384b4816ff74253c222d9386cecbcf4ecf96
Nicholas Curran <nic-curran1@hotmail.com>
2021-07-03 12:48:57 +1000

    Create new friend

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,10 +1,25 @@
 class FriendsController < ApplicationController
-    def index
-        @friends = Friend.all
-    end
+  def index
+    @friends = Friend.all
+  end
 
-    def new
-      # @user = User.find(params[:id])
-      @friend = Friend.new
+  def new
+    @friend = Friend.new
+  end
+
+  def create
+    @friend = Friend.new(friend_params)
+    @friend.user = current_user
+    if @friend.save
+      redirect_to friend_path(@friend)
+    else
+      render :new
     end
+  end
+
+  private
+
+  def friend_params
+    params.require(:friend).permit(:price, :description, :location)
+  end
 end
