6d6361506cede8bc2b5d7a47b70160e14cca6d50
Nicholas Curran <nic-curran1@hotmail.com>
2021-07-03 11:36:53 +1000

    new page created and additional routing for new

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -2,4 +2,9 @@ class FriendsController < ApplicationController
     def index
         @friends = Friend.all
     end
+
+    def new
+      # @user = User.find(params[:id])
+      @friend = Friend.new
+    end
 end
