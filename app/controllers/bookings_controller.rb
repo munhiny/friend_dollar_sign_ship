8fa3389a72878e58c7975afc340cf2d9cdd246fc
Nicholas Curran <nic-curran1@hotmail.com>
2021-07-06 20:43:01 +1000

    We got a dashboard

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -26,6 +26,6 @@ class BookingsController < ApplicationController
   private
 
   def booking_params
-    params.require(:booking).permit(:start_time, :end_time)
+    params.require(:booking).permit(:start_time, :end_time, :date)
   end
 end
