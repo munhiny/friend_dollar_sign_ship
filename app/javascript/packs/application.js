ea82b1de3789fc3cd9b33377dbeba1800ca796f8
Nicholas Curran <nic-curran1@hotmail.com>
2021-07-06 19:17:33 +1000

    Yeah boi

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,6 +24,7 @@ require("channels")
 
 // External imports
 import "bootstrap";
+import flatpickr from "flatpickr";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
